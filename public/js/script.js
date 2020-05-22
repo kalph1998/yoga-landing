33699226270c51bd0c983a62938d3259733d3890
kalph <kalpupadhyay@gmail.com>
2020-05-22 14:51:13 +0530

    firebase added

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,21 @@ const checkBoxHandler = () => {
   const t = (checkbox.checked = !checkbox.checked);
   return t;
 };
+
+//adding form
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const details = {
+    name: form.name.value,
+    Email: form.email.value,
+  };
+  db.collection("form")
+    .add(details)
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+});
